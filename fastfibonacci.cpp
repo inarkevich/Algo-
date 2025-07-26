84b679702e83231e57a6259a7528b01b7a2ebafe
Leyla <inarkevich@gmail.com>
2025-07-26 16:03:11 +0100

    Updated Code

--- fastfibonacci.cpp
+++ fastfibonacci.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-
-long long int *array = new long long int[];
-
+long long int *array = nullptr;  
 long long int fibonacci(int n){
+   
     long long int answer =0;
-   // if (array[n]==array[n]){return array[n];}
-    if (n==0){array[n] =0;
+    if (array[n]>=0)
+    {return array[n];}  
+    else if (n==0){array[n] =0;
         return array[n];}
     else if(n==1){array[n] = 1;
         return array[n];}
     else {answer = fibonacci(n-1)+fibonacci (n-2);
         array[n] = answer;
-        return array[n];} 
-        delete [] array;
+        return array[n];}    
 }
-
-      
-
-
-
-
-
-
-
-
-
-
 
 int main (){
 
     int userNumber = 0;
     std::cout<<"Введите число для последовательности Фибоначчи: ";
     std::cin>>userNumber;
+    array = new long long int [userNumber+1];
+    for (int i=0;i<userNumber+1;i++){
+        array[i] = -1;
+    }
     fibonacci(userNumber);
     std::cout<<"Последовательность Фибоначчи: ";
-    for (int i=0;i<userNumber; i++){ {std::cout<<fibonacci(i)<<" ";}}
+    for (int i=0;i<userNumber+1; i++){ {std::cout<<fibonacci(i)<<" ";}}
     std::cout<<std::endl;
+    delete [] array;  
     
 }
